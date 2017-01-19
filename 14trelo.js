73f6cb18a11e06b68d3caf42e0db85e38e4ebdef
Janusz <jr1@interia.pl>
2017-01-19 09:19:31 +0100

    ost

--- 14trelo.js
+++ 14trelo.js
@@ -1,71 +1,135 @@
-$(function() {
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var myHeaders = {
+  'X-Client-Id': 685,
+  'X-Auth-Token': "b724010fbc4c28c30b4842fc36261d9c"
+};
 
-  function randomString() {
-    var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ';
-    var str = '';
-    var i = 0;
-    for (i = 0; i < 10; i++) {
-      str += chars[Math.floor(Math.random() * chars.length)];
-    }
-    return str;
+$.ajaxSetup({
+  headers: myHeaders
+});
+
+$.ajax({
+  url: baseUrl + '/board',
+  method: 'GET',
+  success: function(response) {
+    setupColumns(response.columns);
   }
+});
 
-  function Column(name) {
-    var self = this; // przyda się dla funkcji zagnieżdżonych
+function setupColumns(columns) {
+  columns.forEach(function(column) {
+    var col = new Column(column.id, column.name);
+    board.createColumn(col);
+    setupCards(col, column.cards);
+  });
+}
 
-    this.id = randomString();
-    this.name = name;
-    this.$element = createColumn();
+function setupCards(col, cards) {
+  cards.forEach(function(card) {
+    var card = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
+    
+    col.addCard(card);
+  })
+}
 
-    function createColumn() {
-      // TWORZENIE ELEMENTÓW SKŁADOWYCH KOLUMNY
-      var $column = $('<div>').addClass('column');
-      var $columnTitle = $('<h2>').addClass('column-title').text(self.name);
-      var $columnCardList = $('<ul>').addClass('column-list');
-      var $columnDelete = $('<button>').addClass('btn-delete').text('X');
-      var $columnAddCard = $('<button>').addClass('add-card').text('Dodaj zadanie');
+//$(function() {
 
-      // PODPINANIE ODPOWIEDNICH ZDARZEŃ
-      $columnDelete.click(function() {
-        self.removeColumn();
+function randomString() {
+  var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ';
+  var str = '';
+  var i = 0;
+  for (i = 0; i < 10; i++) {
+    str += chars[Math.floor(Math.random() * chars.length)];
+  }
+  return str;
+}
+
+function Column(id, name) {
+  var self = this; // przyda się dla funkcji zagnieżdżonych
+
+  this.id = id;
+  this.name = name;
+  this.$element = createColumn();
+
+  function createColumn() {
+    // TWORZENIE ELEMENTÓW SKŁADOWYCH KOLUMNY
+    var $column = $('<div>').addClass('column');
+    var $columnTitle = $('<h2>').addClass('column-title').text(self.name);
+    var $columnCardList = $('<ul>').addClass('column-list');
+    var $columnDelete = $('<button>').addClass('btn-delete').text('X');
+    var $columnAddCard = $('<button>').addClass('add-card').text('Dodaj zadanie');
+
+    // PODPINANIE ODPOWIEDNICH ZDARZEŃ
+    $columnDelete.click(function() {
+      self.removeColumn();
+    });
+    $columnAddCard.click(function(event) {
+      var cardName = prompt("Wpisz nazwę karty");
+      event.preventDefault();
+
+      $.ajax({
+        url: baseUrl + '/card',
+        method: 'POST',
+        data: {
+          name: cardName,
+          bootcamp_kanban_column_id: self.id
+        },
+        success: function(response) {
+          var card = new Card(response.id, cardName);
+          self.addCard(card);
+        }
       });
-      $columnAddCard.click(function(event) {
-        self.addCard(new Card(prompt("Wpisz nazwę zadania")));
-      });
+    });
+    // KONSTRUOWANIE ELEMENTU KOLUMNY
+    $column.append($columnTitle)
+      .append($columnDelete)
+      .append($columnAddCard)
+      .append($columnCardList);
 
-      // KONSTRUOWANIE ELEMENTU KOLUMNY
-      $column.append($columnTitle)
-        .append($columnDelete)
-        .append($columnAddCard)
-        .append($columnCardList);
-
-      // ZWRACANIE STWORZONEJ  KOLUMNY
-      return $column;
-    }
+    // ZWRACANIE STWORZONEJ  KOLUMNY
+    return $column;
   }
-  Column.prototype = {
-    addCard: function(card) {
-      this.$element.children('ul').append(card.$element);
-    },
-    removeColumn: function() {
-      this.$element.remove();
-    }
-
-  };
-  Card.prototype = {
-    removeCard: function() {
-      this.$element.remove();
-    }
-  }
-
-  function Card(description) {
+}
+Column.prototype = {
+  addCard: function(card) {
+    this.$element.children('ul').append(card.$element);
+  },
+  removeColumn: function() {
     var self = this;
+    $.ajax({
+      url: baseUrl + '/column/' + self.id,
+      method: 'DELETE',
+      success: function() {
 
-    this.id = randomString();
-    this.description = description;
-    this.$element = createCard(); //
+        self.$element.remove();
 
-function createCard() {
+      }
+    });
+  }
+
+};
+Card.prototype = {
+  removeCard: function() {
+    var self = this;
+     $.ajax({
+      url: baseUrl + '/card/' + self.id,
+      method: 'DELETE',
+      success: function(){
+        self.$element.remove();
+      }
+    });
+}}
+
+function Card(id, description) {
+  var self = this;
+   
+   
+
+  this.id = id;
+  this.description = description;
+  this.$element = createCard(); //
+
+  function createCard() {
     // TWORZENIE KLOCKÓW
     var $card = $('<li>').addClass('card');
     var $cardDescription = $('<p>').addClass('card-description').text(self.description);
@@ -83,39 +147,40 @@ function createCard() {
     return $card;
   }
 
+}
 
-  }
+function initSortable() {
+  $('.column-list').sortable({
+    connectWith: '.column-list',
+    placeholder: 'card-placeholder'
+  }).disableSelection();
+}
+var board = {
+  name: 'Tablica Kanban',
 
-  
-  var board = {
-    name: 'Tablica Kanban',
-    addColumn: function(column) {
-      this.$element.append(column.$element);
-      initSortable();
-    },
-    $element: $('#board .column-container'),
-
-      addColumn: function(column) {
-      this.$element.append(column.$element);
-      initSortable(); 
-    }
-
-  };
-
-  function initSortable() {
-    $('.column-list').sortable({
-      connectWith: '.column-list',
-      placeholder: 'card-placeholder'
-    }).disableSelection();
-  }
-  $('.create-column')
-    .click(function() {
-      var name = prompt('Wpisz nazwę kategorii');
-      var column = new Column(name);
-      board.addColumn(column);
+  createColumn: function(column) {
+    this.element.append(column.$element);
+    initSortable();
+  },
+  element: $('#board .column-container')
+};
+$('.create-column')
+  .click(function() {
+    var columnName = prompt('Wpisz nazwę kolumny');
+    $.ajax({
+      url: baseUrl + '/column',
+      method: 'POST',
+      data: {
+        name: columnName
+      },
+      success: function(response) {
+        var column = new Column(response.id, columnName);
+        board.createColumn(column);
+      }
     });
-  // TWORZENIE KOLUMN
-  var todoColumn = new Column('Do zrobienia');
+  });
+// TWORZENIE KOLUMN
+/* var todoColumn = new Column('Do zrobienia');
   var doingColumn = new Column('W trakcie');
   var doneColumn = new Column('Skończone');
 
@@ -130,4 +195,4 @@ function createCard() {
   todoColumn.addCard(card1);
   doingColumn.addCard(card2);
 
-});
+});*/
